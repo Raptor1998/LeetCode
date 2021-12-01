286835996762f826b1b750dd89240d72b2ff7aac
Raptor1998 <knightcwh@163.com>
2021-12-01 22:22:08 +0800

    https://leetcode-cn.com/problems/squares-of-a-sorted-array/

--- src/main/java/com/raptor/base/daytwo/Solution977.java
+++ src/main/java/com/raptor/base/daytwo/Solution977.java
@@ -1,5 +1,9 @@
 package com.raptor.base.daytwo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * @author raptor
  * @description Solution977
@@ -8,10 +12,38 @@ package com.raptor.base.daytwo;
  */
 public class Solution977 {
     public static int[] sortedSquares(int[] nums) {
-        return null;
+        int l = nums.length - 1;
+        int[] res = new int[l + 1];
+        int i = 0, j = l;
+        while (l >= 0 && i <= j) {
+            if (Math.abs(nums[i]) < Math.abs(nums[j])) {
+                res[l] = nums[j] * nums[j];
+                j--;
+                l--;
+            } else if (Math.abs(nums[i]) > Math.abs(nums[j])) {
+                res[l] = nums[i] * nums[i];
+                i++;
+                l--;
+            } else {
+                res[l] = nums[i] * nums[i];
+                i++;
+                l--;
+                if (i < j) {
+                    res[l] = nums[j] * nums[j];
+                    j--;
+                    l--;
+                }
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
-
+        int[] nums = new int[]{-7, -3, 2, 2, 2};
+        int[] res = sortedSquares(nums);
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
+        }
     }
+
 }
