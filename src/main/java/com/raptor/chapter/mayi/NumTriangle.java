2ba1f86dee2d049316bbb0c0d12ac7fee0360b39
Raptor1998 <knightcwh@163.com>
2022-03-02 18:13:44 +0800

    mayi

--- src/main/java/com/raptor/chapter/mayi/NumTriangle.java
+++ src/main/java/com/raptor/chapter/mayi/NumTriangle.java
@@ -7,29 +7,50 @@ package com.raptor.chapter.mayi;
  */
 public class NumTriangle {
     public static int minimumTotal(int[][] triangle) {
-        // write your code here
+        if (triangle == null || triangle.length == 0) {
+            return -1;
+        }
+        if (triangle[0] == null || triangle[0].length == 0) {
+            return -1;
+        }
+
+        // state: dp[x][y] = minimum path value from 0,0 to x,y
         int n = triangle.length;
-        int dp[][] = new int[n][n];
-        int maxTotal = triangle[0][0];
+        int[][] dp = new int[n][n];
+
+        // initialize 
         dp[0][0] = triangle[0][0];
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j < triangle[i].length; j++) {
-                if (j != 0) {
-                    dp[i][j] = Math.min(dp[i - 1][j] + triangle[i][j], dp[i - 1][j - 1] + triangle[i][j]);
-                } else {
-                    dp[i][j] = dp[i - 1][j] + triangle[i][j];
-                }
+            dp[i][0] = dp[i - 1][0] + triangle[i][0];
+            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
+        }
+
+        print(dp);
+
+        // top down
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j];
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < dp[i].length; j++) {
-                System.out.print(dp[i][j] + " ");
+        // answer
+        int best = dp[n - 1][0];
+        for (int i = 1; i < n; i++) {
+            best = Math.min(best, dp[n - 1][i]);
+        }
+        return best;
+    }
+
+    public static void print(int a[][]) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + " ");
             }
             System.out.println();
         }
-        return maxTotal;
     }
+
 
     public static void main(String[] args) {
         int[][] a = {
